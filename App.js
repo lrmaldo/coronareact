08632d2233665fc44552f5ca7a19c7792b2c9297
Ing Leonardo <lrmaldo@gmail.com>
2020-03-20 17:20:59 -0600

    mapa listo con api

--- App.js
+++ App.js
@@ -1,12 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View,
+   AppRegistry,
+   ScrollView,
+   Animated,
+   Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      markers: [],
+    };
+  }
+
+  fetchMarkerData() {
+    fetch('http://mapa.sattlink.com/api')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ 
+          isLoading: false,
+          markers: responseJson, 
+        });
+        console.log(responseJson);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  componentDidMount() {
+    this.fetchMarkerData();
+}
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle}  
+        initialRegion={{
+          latitude: 18.0478381,
+      longitude: -96.1628699,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+        }}>
+
+{this.state.isLoading ? null : this.state.markers.map((marker, index) => {
+     const coords = {
+         latitude: marker.lat,
+         longitude: marker.long,
+     };
+
+     const metadata = `Confirmados: ${marker.confirmados}
+     -Negativos: ${marker.negativos}
+     -Recuperados: ${marker.recuperados}`;
+    
+     return (
+         <MapView.Marker
+            key={index}
+            coordinate={coords}
+            title={marker.region}
+            //description={metadata}
+         >
+          
+         </MapView.Marker>
+         
+       
+
+     );
+  })}
+    </MapView>
+
+
+
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +87,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
+
+/**
+ *  Keystore password: 25a78909d91d43689e1b931f29b89220
+  Key alias:         QGxybWFsZG8vTWFwc0Nvcm8=
+  Key password:      7b1d9fc0c21d4b49b6cd1b4a3c5b5a37
+ */
