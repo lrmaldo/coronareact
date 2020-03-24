a6a2bfc8f15d7f09160619e730aa0df1a5a241a8
Ing Leonardo <lrmaldo@gmail.com>
2020-03-24 11:43:45 -0600

    mapa listo

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ import { StyleSheet, Text, View,
    Animated,
    Dimensions } from 'react-native';
 
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = height / 4;
+const CARD_WIDTH = CARD_HEIGHT - 20;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -30,20 +35,65 @@ export default class App extends React.Component {
         console.log(error);
       });
   }
+
+  componentWillMount() {
+    this.index = 0;
+    this.animation = new Animated.Value(0);
+  }
   componentDidMount() {
     this.fetchMarkerData();
+
+    this.animation.addListener(({value})=>{
+      let index = Math.floor(value / CARD_WIDTH + 0.3);
+      if(index >= this.state.markers.length){
+        index = this.state.markers - 1;
+      }
+      if (index <= 0){
+        index = 0;
+      }
+      clearTimeout(this.regionTimeout)
+      this.regionTimeout = setTimeout(() => {
+          if (this.index !== index) {
+              this.index = index
+              const { coordinate } = this.state.markers[index]
+             
+          }
+      }, 10)
+
+    });
 }
 
 
   render() {
+    
+    const interpolations = this.state.markers.map((marker,index)=>{
+      const inputRange =[
+        (index -1) * CARD_WIDTH,
+      index * CARD_WIDTH,
+      (index +1) * CARD_WIDTH
+      ]
+      const scale = this.animation.interpolate({
+        inputRange,
+        outputRange:[2,5,2],
+        extrapolate:"clamp"
+      })
+      const opacity = this.animation.interpolate({
+        inputRange,
+        outputRange:[.35,1,.35],
+        extrapolate:"clamp",
+      })
+      return {scale,opacity}
+    })
+
+
     return (
       <View style={styles.container}>
         <MapView style={styles.mapStyle}  
         initialRegion={{
-          latitude: 18.0478381,
-      longitude: -96.1628699,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+          latitude: 17.0829383,
+      longitude: -96.7884567,
+      latitudeDelta: 3.622,
+      longitudeDelta: 3.621,
         }}>
 
 {this.state.isLoading ? null : this.state.markers.map((marker, index) => {
@@ -55,14 +105,31 @@ export default class App extends React.Component {
      const metadata = `Confirmados: ${marker.confirmados}
      -Negativos: ${marker.negativos}
      -Recuperados: ${marker.recuperados}`;
-    
+     
+     const scaleStyle ={
+       transform: [
+         {
+           scale:interpolations[index].scale
+         }
+       ]
+     }
+     const opacityStyle ={
+       opacity:interpolations[index].opacity
+     }
+
      return (
          <MapView.Marker
             key={index}
             coordinate={coords}
-            title={marker.region}
+            
+            //title={marker.region}
             //description={metadata}
+            
          >
+          <Animated.View style={[styles.markerWrap,opacityStyle]}>
+          <Animated.View style={[styles.ring,scaleStyle]} />
+          <View style={styles.marker} />
+        </Animated.View>
           
          </MapView.Marker>
          
@@ -71,9 +138,48 @@ export default class App extends React.Component {
      );
   })}
     </MapView>
-
-
-
+  
+    <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: this.animation,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+           {this.state.markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+            
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>{marker.region}</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  Confirmados: {marker.confirmados}
+                </Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  Sospechosos: {marker.sospechosos}
+                </Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  Negativos: {marker.negativos}
+                </Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  Muertos: {marker.muertos}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </Animated.ScrollView>
         
       </View>
     );
@@ -91,6 +197,62 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+   scrollView: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  }, endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  textContent: {
+    flex: 1,
+  },
+  cardtitle: {
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marker: {
+    width: 16,
+    height: 16,
+    borderRadius: 70,
+    backgroundColor: "rgba(130,4,150, 0.9)",
+  },
+  ring: {
+    width: 48,
+    height: 48,
+    borderRadius:60,
+    backgroundColor: "rgba(130,4,150, 0.3)",
+    position: "absolute",
+    borderWidth: 4,
+    borderColor: "rgba(130,4,150, 0.5)",
+  }, 
+
+
+
 });
 
 /**
